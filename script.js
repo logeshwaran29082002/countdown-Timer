b4c5040f66b259752b15754cefdf343ee8bec36a
Logeshwaran <logurashitha2002@gmail.com>
2025-12-01 16:47:57 +0530

    script is finesh

---
+++ script.js
@@ -0,0 +1,34 @@
+const days =document.querySelector("#days")
+const hours=document.querySelector("#hours") 
+const minutes =document.querySelector("#minutes")
+const seconds=document.querySelector("#seconds") 
+
+
+
+
+function counDown(){
+    const currentYear = new Date().getFullYear();
+
+const newYear = new Date(`January 1 ${currentYear+1} 00:00:00`);
+
+const currentDate = new Date();
+console.log(currentDate);
+
+ const diff = newYear - currentDate;
+ 
+
+ const d =Math.floor(diff/1000/60/60/24);
+ console.log(d);
+ const h = Math.floor((diff/1000/60/60)%24);
+ console.log(h);
+ const m = Math.floor((diff/1000/60/60)%60);
+ console.log(m);
+ const s = Math.floor((diff/1000)%60);
+ console.log(s);
+ days.innerHTML=d<10?"0"+d:d;
+ hours.innerHTML=h<10?"0"+h:h;
+ minutes.innerHTML=m<10?"0"+m:m;
+ seconds.innerHTML=s<10?"0"+s:s;
+}
+
+setInterval(counDown,1000)
